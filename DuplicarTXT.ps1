7b487039347290f673be143dd57177ff8160ded4
Jhonny_Alejandro_Montoya_Montoya <jhonnyam16@gmail.com>
2022-06-22 22:28:21 -0500

    Fecha del sistema

--- DuplicarTXT.ps1
+++ DuplicarTXT.ps1
@@ -1,4 +1,6 @@
 Copy-Item -Filter *.txt -Path c:\Jhonny -Recurse -Destination C:\temp\text -Force
 $Varaux = "Jhonny"
 Get-LocalGroup -Name Administradores        #Esta linea es para sacar los administradores del PC
+
 Write-Host "Se duplico el archivo exitosamen..." $Varaux
+Get-Datecls
