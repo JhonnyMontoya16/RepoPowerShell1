bc4a9f9c8e166e3ea89e10a1e941d032b95ef02a
Jhonny_Alejandro_Montoya_Montoya <jhonnyam16@gmail.com>
2022-06-22 21:24:38 -0500

    Comentario y linea de administradora

--- DuplicarTXT.ps1
+++ DuplicarTXT.ps1
@@ -1,3 +1,4 @@
 Copy-Item -Filter *.txt -Path c:\Jhonny -Recurse -Destination C:\temp\text -Force
-
-Write-Host "Se duplico el archivo exitosamen..."
+$Varaux = "Jhonny"
+Get-LocalGroup -Name Administradores        #Esta linea es para sacar los administradores del PC
+Write-Host "Se duplico el archivo exitosamen..." $Varaux
